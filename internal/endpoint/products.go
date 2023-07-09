d1eb924ee22bedbbf7660a539ac8f793d0c814f7
Sergey Sustov <darkren92@gmail.com>
2023-07-09 23:22:57 +0300

    feat: implement products API

--- internal/endpoint/products.go
+++ internal/endpoint/products.go
@@ -1,1 +1,349 @@
 package endpoint
+
+import (
+	"io/ioutil"
+	"net/http"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/sirupsen/logrus"
+
+	"github.com/Darkren/getmark-home/pkg/data/product"
+	"github.com/Darkren/getmark-home/pkg/data/user"
+	"github.com/Darkren/getmark-home/pkg/service/auth"
+	"github.com/Darkren/getmark-home/pkg/service/pricetag"
+)
+
+func AddProduct(log *logrus.Logger, authService auth.Service,
+	usersRepo user.Repository, productsRepo product.Repository) func(gctx *gin.Context) {
+	return func(gctx *gin.Context) {
+		log := log.WithFields(logrus.Fields{"endpoint": "AddProduct"})
+
+		login, ok := mustValidateToken(gctx, log, authService)
+		if !ok {
+			return
+		}
+
+		log = log.WithFields(logrus.Fields{"login": login})
+
+		var p product.Product
+		if err := gctx.Bind(&p); err != nil {
+			gctx.AbortWithStatus(http.StatusBadRequest)
+			return
+		}
+
+		user, err := usersRepo.UserByLogin(login)
+		if err != nil {
+			log.Errorf("usersRepo.UserByLogin: %v\n", err)
+			gctx.AbortWithStatus(http.StatusInternalServerError)
+			return
+		}
+
+		if user == nil {
+			gctx.AbortWithStatus(http.StatusForbidden)
+			return
+		}
+
+		p.UserID = user.ID
+
+		if err := productsRepo.Add(&p); err != nil {
+			log.Errorf("productsRepo.Add: %v\n", err)
+			gctx.AbortWithStatus(http.StatusInternalServerError)
+			return
+		}
+
+		gctx.Status(http.StatusOK)
+	}
+}
+
+func DeleteProduct(log *logrus.Logger, authService auth.Service,
+	usersRepo user.Repository, productsRepo product.Repository) func(gctx *gin.Context) {
+	return func(gctx *gin.Context) {
+		log := log.WithFields(logrus.Fields{"endpoint": "DeleteProduct"})
+
+		login, ok := mustValidateToken(gctx, log, authService)
+		if !ok {
+			return
+		}
+
+		log = log.WithFields(logrus.Fields{"login": login})
+
+		barcode := gctx.Request.URL.Query().Get("barcode")
+		if barcode == "" {
+			gctx.AbortWithStatus(http.StatusBadRequest)
+			return
+		}
+
+		user, err := usersRepo.UserByLogin(login)
+		if err != nil {
+			log.Errorf("usersRepo.UserByLogin: %v\n", err)
+			gctx.AbortWithStatus(http.StatusInternalServerError)
+			return
+		}
+
+		if user == nil {
+			gctx.AbortWithStatus(http.StatusForbidden)
+			return
+		}
+
+		if err := productsRepo.Delete(barcode, user.ID); err != nil {
+			log.WithFields(logrus.Fields{
+				"barcode": barcode,
+				"userID":  user.ID,
+			}).Errorf("productsRepo.Delete: %v\n", err)
+			gctx.AbortWithStatus(http.StatusInternalServerError)
+			return
+		}
+
+		gctx.Status(http.StatusOK)
+	}
+}
+
+func ListProducts(log *logrus.Logger, authService auth.Service,
+	usersRepo user.Repository, productsRepo product.Repository) func(gctx *gin.Context) {
+	return func(gctx *gin.Context) {
+		log := log.WithFields(logrus.Fields{"endpoint": "ListProducts"})
+
+		login, ok := mustValidateToken(gctx, log, authService)
+		if !ok {
+			return
+		}
+
+		log = log.WithFields(logrus.Fields{"login": login})
+
+		user, err := usersRepo.UserByLogin(login)
+		if err != nil {
+			log.Errorf("usersRepo.UserByLogin: %v\n", err)
+			gctx.AbortWithStatus(http.StatusInternalServerError)
+			return
+		}
+
+		if user == nil {
+			gctx.AbortWithStatus(http.StatusForbidden)
+			return
+		}
+
+		products, err := productsRepo.List(user.ID)
+		if err != nil {
+			log.WithFields(logrus.Fields{"userID": user.ID}).Errorf("productsRepo.List: %v\n", err)
+			gctx.AbortWithStatus(http.StatusInternalServerError)
+			return
+		}
+
+		if len(products) == 0 {
+			gctx.AbortWithStatus(http.StatusNotFound)
+			return
+		}
+
+		gctx.JSON(http.StatusOK, &products)
+	}
+}
+
+func GetProduct(log *logrus.Logger, authService auth.Service,
+	usersRepo user.Repository, productsRepo product.Repository) func(gctx *gin.Context) {
+	return func(gctx *gin.Context) {
+		log := log.WithFields(logrus.Fields{"endpoint": "GetProduct"})
+
+		login, ok := mustValidateToken(gctx, log, authService)
+		if !ok {
+			return
+		}
+
+		log = log.WithFields(logrus.Fields{"login": login})
+
+		user, err := usersRepo.UserByLogin(login)
+		if err != nil {
+			log.Errorf("usersRepo.UserByLogin: %v\n", err)
+			gctx.AbortWithStatus(http.StatusInternalServerError)
+			return
+		}
+
+		if user == nil {
+			gctx.AbortWithStatus(http.StatusForbidden)
+			return
+		}
+
+		barcode := gctx.Param("barcode")
+
+		product, err := productsRepo.Product(barcode, user.ID)
+		if err != nil {
+			log.WithFields(logrus.Fields{
+				"barcode": barcode,
+				"userID":  user.ID,
+			}).Errorf("productsRepo.Product: %v\n", err)
+			gctx.AbortWithStatus(http.StatusInternalServerError)
+			return
+		}
+
+		if product == nil {
+			gctx.AbortWithStatus(http.StatusNotFound)
+			return
+		}
+
+		gctx.JSON(http.StatusOK, &product)
+	}
+}
+
+func GeneratePriceTag(log *logrus.Logger, authService auth.Service,
+	usersRepo user.Repository, productsRepo product.Repository,
+	priceTagService pricetag.Service) func(gctx *gin.Context) {
+	return func(gctx *gin.Context) {
+		log := log.WithFields(logrus.Fields{"endpoint": "GeneratePriceTag"})
+
+		login, ok := mustValidateToken(gctx, log, authService)
+		if !ok {
+			return
+		}
+
+		log = log.WithFields(logrus.Fields{"login": login})
+
+		user, err := usersRepo.UserByLogin(login)
+		if err != nil {
+			log.Errorf("usersRepo.UserByLogin: %v\n", err)
+			gctx.AbortWithStatus(http.StatusInternalServerError)
+			return
+		}
+
+		if user == nil {
+			gctx.AbortWithStatus(http.StatusForbidden)
+			return
+		}
+
+		barcode := gctx.Param("barcode")
+
+		product, err := productsRepo.Product(barcode, user.ID)
+		if err != nil {
+			log.WithFields(logrus.Fields{
+				"barcode": barcode,
+				"userID":  user.ID,
+			}).Errorf("productsRepo.Product: %v\n", err)
+			gctx.AbortWithStatus(http.StatusInternalServerError)
+			return
+		}
+
+		if product == nil {
+			gctx.AbortWithStatus(http.StatusNotFound)
+			return
+		}
+
+		log = log.WithFields(logrus.Fields{
+			"barcode": barcode,
+			"name":    product.Name,
+			"cost":    product.Cost,
+		})
+
+		tagContentsReader, err := priceTagService.Generate(barcode, product.Name, product.Cost)
+		if err != nil {
+			log.Errorf("priceTagService.Generate: %v\n", err)
+			gctx.AbortWithStatus(http.StatusInternalServerError)
+			return
+		}
+
+		tagContents, err := ioutil.ReadAll(tagContentsReader)
+		if err != nil {
+			log.Errorf("ioutil.ReadAll: %v\n", err)
+			gctx.AbortWithStatus(http.StatusInternalServerError)
+			return
+		}
+
+		filePath := "tags/doc_" + barcode + "_" + strconv.FormatInt(time.Now().UTC().Unix(), 10) + ".pdf"
+		err = os.WriteFile(filePath, tagContents, 0644)
+		if err != nil {
+			log.Errorf("os.WriteFile: %v\n", err)
+			gctx.AbortWithStatus(http.StatusInternalServerError)
+			return
+		}
+
+		gctx.File(filePath)
+	}
+}
+
+func GetPriceTag(log *logrus.Logger, authService auth.Service,
+	usersRepo user.Repository, productsRepo product.Repository) func(gctx *gin.Context) {
+	return func(gctx *gin.Context) {
+		log := log.WithFields(logrus.Fields{"endpoint": "GeneratePriceTag"})
+
+		login, ok := mustValidateToken(gctx, log, authService)
+		if !ok {
+			return
+		}
+
+		log = log.WithFields(logrus.Fields{"login": login})
+
+		user, err := usersRepo.UserByLogin(login)
+		if err != nil {
+			log.Errorf("usersRepo.UserByLogin: %v\n", err)
+			gctx.AbortWithStatus(http.StatusInternalServerError)
+			return
+		}
+
+		if user == nil {
+			gctx.AbortWithStatus(http.StatusForbidden)
+			return
+		}
+
+		filePath := gctx.Param("filepath")
+		if filePath == "" {
+			gctx.AbortWithStatus(http.StatusNotFound)
+			return
+		}
+
+		filePathTokens := strings.Split(filePath, "_")
+		if len(filePathTokens) != 3 || filePathTokens[0] != "doc" {
+			gctx.AbortWithStatus(http.StatusNotFound)
+			return
+		}
+
+		barcode := filePathTokens[1]
+
+		product, err := productsRepo.Product(barcode, user.ID)
+		if err != nil {
+			log.WithFields(logrus.Fields{
+				"barcode": barcode,
+				"userID":  user.ID,
+			}).Errorf("productsRepo.Product: %v\n", err)
+			gctx.AbortWithStatus(http.StatusInternalServerError)
+			return
+		}
+
+		if product == nil {
+			gctx.AbortWithStatus(http.StatusNotFound)
+			return
+		}
+
+		gctx.File("tags/" + filePath)
+	}
+}
+
+func mustValidateToken(gctx *gin.Context, log *logrus.Entry, authService auth.Service) (string, bool) {
+	tokenStr := gctx.Request.Header.Get("Authorization")
+	if tokenStr == "" {
+		gctx.AbortWithStatus(http.StatusBadRequest)
+		return "", false
+	}
+
+	token, err := auth.NewToken(tokenStr)
+	if err != nil {
+		gctx.AbortWithStatus(http.StatusBadRequest)
+		return "", false
+	}
+
+	if err := authService.ValidateToken(token); err != nil {
+		gctx.AbortWithStatus(http.StatusForbidden)
+		return "", false
+	}
+
+	login, err := token.Login()
+	if err != nil {
+		log.WithFields(logrus.Fields{"token": tokenStr}).Errorf("login is missing in the token")
+		// auth service validated token, so the fact that there's no login there
+		// most likely means we messed it up somewhere pretty bad
+		gctx.AbortWithStatus(http.StatusInternalServerError)
+		return "", false
+	}
+
+	return login, true
+}
